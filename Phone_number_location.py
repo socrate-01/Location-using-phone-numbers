8b5a8a3fb2edc0addd06e3556b8e24f4b9b50ba7
Socratis <Seydinamouhamadousanoumbaye@esp.sn>
2022-08-08 17:13:48 +0000

    Add files via upload

--- Phone_number_location.py
+++ Phone_number_location.py
@@ -1,1 +1,9 @@
+import phonenumbers
+from test_phone import number
+from phonenumbers import carrier
+from phonenumbers import geocoder
 
+ch_numbers = phonenumbers.parse(number, "CH")
+print(geocoder.description_for_number(ch_numbers, "en"))
+service_number = phonenumbers.parse(number,"RO")
+print(carrier.name_for_number(service_number,"en"))
--- test_phone.py
+++ test_phone.py
@@ -1,1 +1,3 @@
-
+#number = "+221-76-203-12-51"
+print("Number format\nex : +CountryCodeNumber-XX-XXX-XX-XX")
+number = input("Please enter a cellphone number : ")
